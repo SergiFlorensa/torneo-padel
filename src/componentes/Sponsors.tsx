64bddd2743dc38cd6fab36af84ae87fde474e5f1
Sergi Florensa <sergiflorensadj@gmail.com>
2025-08-24 11:55:19 +0200

    padel

--- src/componentes/Sponsors.tsx
+++ src/componentes/Sponsors.tsx
@@ -1,6 +1,7 @@
 const Sponsors: React.FC = () => (
   <footer className="bg-gray-100 py-8 text-center">
-    <p className="text-gray-500">Patrocinadores próximamente</p>
+    <p className="text-gray-500">🎾 Patrocinadores confirmados muy pronto 🎉</p>
   </footer>
 );
+
 export default Sponsors;
