beb6135c342281b1c37af73cbad2ac7fa2e06ed5
Sergi Florensa <sergiflorensadj@gmail.com>
2025-09-09 19:47:28 +0200

    padel

--- src/componentes/Sponsors.tsx
+++ src/componentes/Sponsors.tsx
@@ -1,6 +1,14 @@
 const Sponsors: React.FC = () => (
-  <footer className="bg-gray-100 py-8 text-center">
-    <p className="text-gray-500">🎾 Patrocinadores confirmados muy pronto 🎉</p>
+  <footer className="bg-black py-8 px-4">
+    <div className="flex flex-wrap items-center justify-center gap-8">
+      <img src="/patrocinadores/piee1.png" alt="La Piemontesa" className="h-12 object-contain" />
+      <img src="/patrocinadores/tooot.png" alt="Tot Padel" className="h-12 object-contain" />
+      <img src="/patrocinadores/marde11.png" alt="Mary de Babys" className="h-12 object-contain" />
+      <img src="/patrocinadores/wwalls.png" alt="Valls Design" className="h-12 object-contain" />
+      <img src="/patrocinadores/onA1.png" alt="On Air" className="h-12 object-contain" />
+      <img src="/patrocinadores/victo1.png" alt="Nueva Victoria" className="h-12 object-contain" />
+      <img src="/patrocinadores/capra1.png" alt="Caprabo" className="h-12 object-contain" />
+    </div>
   </footer>
 );
 
